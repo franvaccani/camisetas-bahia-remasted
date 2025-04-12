abae9ead8b32c2093b6634912578e17fcc31a410
franvaccani <franciscovaccani08@gmail.com>
2025-04-12 15:36:11 -0300

    cambios en .env

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -58,7 +58,6 @@ const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
               onChange={(e) => setEmail(e.target.value)}
               className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-black"
               required
-              placeholder="admin@camisetasbahia.com"
             />
           </div>
           
@@ -73,7 +72,6 @@ const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
               onChange={(e) => setPassword(e.target.value)}
               className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-black"
               required
-              placeholder="••••••••"
             />
           </div>
           
